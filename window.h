85467a7e425b957a8dacdd14f9d34bdc116e3611
GreeenieQWQ <55385374+GreenieQwQ@users.noreply.github.com>
2019-09-21 17:47:38 +0800

    upload comment

--- window.h
+++ window.h
@@ -12,7 +12,7 @@ public:
     void loadCustomer(customer); //服务一个新的顾客并将等待时间设置为此顾客的服务时间；
     void dealing(); // 窗口等待时间减少一个单位;
     void increaseNumber()
-    { dealedNumber++; } //增加服务的人数    
+    { dealedNumber++; } //增加服务过的人数    
     int getId() const
     { return theCustomer.getId(); } //返回服务顾客的顺序号
     int getNumber() const
@@ -26,7 +26,7 @@ private:
     customer theCustomer; //处理的顾客
     int dealingTime; //剩余窗口等待时间
     int dealedNumber; //服务的人数
-    void dealing(int time); // 窗口等待时间减少time个单位;    
+    void dealing(int time); // 窗口等待时间减少time个单位；作为备用函数   
 };
 
 window::window(): dealingTime(0), dealedNumber(0)
